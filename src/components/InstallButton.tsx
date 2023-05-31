3c70f7ffd773198384c7fb231c0e3f6bbf493121
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2023-05-31 00:02:55 -0300

    try install button

---
+++ src/components/InstallButton.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+
+interface CustomWindow extends Window {
+  deferredPrompt: any;
+}
+
+const InstallButton: React.FC = () => {
+  const [showInstallButton, setShowInstallButton] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event: Event) => {
+      event.preventDefault();
+      setShowInstallButton(true);
+    };
+
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
+
+  const handleInstall = () => {
+    const promptEvent = new Promise((resolve) => {
+      const handleUserChoice = (choiceResult: any) => {
+        resolve(choiceResult);
+        setShowInstallButton(false);
+      };
+
+      (window as unknown as CustomWindow).addEventListener(
+        "appinstalled",
+        handleUserChoice
+      );
+
+      if ((window as unknown as CustomWindow).deferredPrompt) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        (window as unknown as CustomWindow).deferredPrompt.prompt();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        (window as unknown as CustomWindow).deferredPrompt.userChoice.then(
+          handleUserChoice
+        );
+      }
+    });
+
+    void promptEvent.then((choiceResult: any) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the installation");
+      }
+    });
+  };
+
+  if (!showInstallButton) {
+    return null;
+  }
+
+  return <button onClick={handleInstall}>Install App</button>;
+};
+
+export default InstallButton;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,26 @@
 import { GameSettingsProvider } from "@/context/GameSettings";
 import { type AppType } from "next/dist/shared/lib/utils";
 
-import "@/styles/globals.css";
+import InstallButton from "@/components/InstallButton";
 import { GameStateProvider } from "@/context/GameState";
-import { UIProvider } from "@/context/UI";
 import { RoundsSettingsProvider } from "@/context/RoundsSettings";
-import { useEffect } from "react";
+import { UIProvider } from "@/context/UI";
+import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  useEffect(() => {
-    window.addEventListener("beforeinstallprompt", () =>
-      console.log("NEFOREINSTALLPROMPT EVENT")
-    );
-  }, []);
   return (
-    <GameSettingsProvider>
-      <GameStateProvider>
-        <RoundsSettingsProvider>
-          <UIProvider>
-            <Component {...pageProps} />
-          </UIProvider>
-        </RoundsSettingsProvider>
-      </GameStateProvider>
-    </GameSettingsProvider>
+    <>
+      <GameSettingsProvider>
+        <GameStateProvider>
+          <RoundsSettingsProvider>
+            <UIProvider>
+              <Component {...pageProps} />
+            </UIProvider>
+          </RoundsSettingsProvider>
+        </GameStateProvider>
+      </GameSettingsProvider>
+      <InstallButton />
+    </>
   );
 };
 
