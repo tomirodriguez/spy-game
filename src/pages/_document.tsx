dd6d2dd7c82b49edf6eb754b3f1f9a024edeb67d
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2023-05-31 09:50:20 -0300

    no se que mas hacer

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,18 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="manifest.webmanifest" />
-          {/* <link rel="manifest" href="/manifest.json" /> */}
-          <script
-            async
-            src="https://unpkg.com/pwacompat"
-            crossOrigin="anonymous"
-          ></script>
-          <link
-            rel="icon"
-            type="image/png"
-            href="icon-192x192.png"
-            sizes="192x192"
-          />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
